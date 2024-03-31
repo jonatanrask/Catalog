2d58dc2f2abbd8441d60311733263c7ad543aefc
Jonatan Santos <jonatan.santos@intersite.com.br>
2024-03-31 19:22:00 -0400

    Adicionado sql para teste

--- src/main/kotlin/org/rask/catalog/services/CategoryService.kt
+++ src/main/kotlin/org/rask/catalog/services/CategoryService.kt
@@ -4,12 +4,14 @@ import org.rask.catalog.entities.Category
 import org.rask.catalog.repositories.CategoryRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class CategoryService {
     @Autowired
     private lateinit var repository: CategoryRepository
 
+    @Transactional(readOnly = true)
     fun findAll(): List<Category> {
      return repository.findAll()
     }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_category (name) VALUES ('Appliances');
+INSERT INTO tb_category (name) VALUES ('Sports');
+INSERT INTO tb_category (name) VALUES ('Beauty');
+INSERT INTO tb_category (name) VALUES ('Toys');
+INSERT INTO tb_category (name) VALUES ('Jewelry');
+INSERT INTO tb_category (name) VALUES ('Automotive');
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES ('Electronics');
