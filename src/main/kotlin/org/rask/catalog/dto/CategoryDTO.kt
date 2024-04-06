5af78f88ca42287885fa5268e6a7c911573e2897
Jonatan Santos <jonatan.santos@intersite.com.br>
2024-04-06 08:02:00 -0400

    Alterado o tipo de classe do DTO para data class

--- src/main/kotlin/org/rask/catalog/dto/CategoryDTO.kt
+++ src/main/kotlin/org/rask/catalog/dto/CategoryDTO.kt
@@ -3,34 +3,16 @@ package org.rask.catalog.dto
 import org.rask.catalog.entities.Category
 import java.io.Serializable
 
-class CategoryDTO : Serializable {
-    private var _id: Long = 0
-    private var _name: String? = null
+data class CategoryDTO(
+    val id: Long,
+    val name: String?
+) : Serializable {
     @Transient
     private val serialVersionUID: Long = 1L
 
-    constructor()
-
-    constructor(id: Long, name: String?) {
-        _id = id
-        _name = name
-    }
-
     constructor(entity: Category) : this(entity.id, entity.name)
 
-    var id: Long
-        get() = _id
-        set(value) {
-            _id = value
-        }
-
-    var name: String?
-        get() = _name
-        set(value) {
-            _name = value
-        }
-
     override fun toString(): String {
-        return "CategoryDTO(id=$_id, name=$_name)"
+        return "CategoryDTO(id=$id, name=$name)"
     }
 }
