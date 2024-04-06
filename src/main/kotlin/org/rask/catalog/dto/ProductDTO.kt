7abea4f0582bfaffe8642cd0c87e08deaa013a1b
Jonatan Santos <jonatan.santos@intersite.com.br>
2024-04-06 10:05:53 -0400

    Criado classe e data classe product e adicionado sql referente

---
+++ src/main/kotlin/org/rask/catalog/dto/ProductDTO.kt
@@ -0,0 +1,36 @@
+package org.rask.catalog.dto
+
+import org.rask.catalog.entities.Category
+import org.rask.catalog.entities.Product
+import java.io.Serializable
+import java.time.Instant
+
+data class ProductDTO(
+    val id: Long?,
+    val name: String?,
+    val description: String?,
+    val price: Double?,
+    val imgUrl: String?,
+    val createdAt: Instant?,
+    var categories: List<CategoryDTO> = listOf()
+) : Serializable {
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+
+    constructor(entity: Product) : this(
+        id = entity.id,
+        name = entity.name,
+        description = entity.description,
+        price = entity.price,
+        imgUrl = entity.imgUrl,
+        createdAt = entity.createdAt
+    )
+
+    constructor(entity: Product, categories: Set<Category>) : this(entity) {
+        this.categories = categories.map { CategoryDTO(it) }
+    }
+}
+
+
---
+++ src/main/kotlin/org/rask/catalog/entities/Product.kt
@@ -0,0 +1,111 @@
+package org.rask.catalog.entities
+
+import jakarta.persistence.*
+import java.io.Serializable
+import java.time.Instant
+
+@Entity
+@Table(name = "tb_product")
+class Product : Serializable {
+    @field:Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private var _id: Long? = null
+    @field:Column(name = "name")
+    private var _name: String? = null
+    @field:Column(name = "description", columnDefinition = "TEXT")
+    private var _description: String? = null
+    @field:Column(name = "price")
+    private var _price: Double? = null
+    @field:Column(name = "img_url")
+    private var _imgUrl: String? = null
+
+    @field:Column(name = "created_at" ,columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private var _createdAt: Instant? = null
+
+    @field:Column(name = "updated_at" ,columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private var _updatedAt: Instant? = null
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+        joinColumns = arrayOf(JoinColumn(name = "product_id")),
+        inverseJoinColumns = arrayOf(JoinColumn(name = "category_id")))
+    private var _categories: Set<Category> = hashSetOf()
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+
+    constructor()
+
+    constructor(entity: Product) {
+        _id = entity.id
+        _name = entity.name
+        _description = entity.description
+        _price = entity.price
+        _imgUrl = entity.imgUrl
+    }
+
+    constructor(id: Long?, name: String?, description: String?, price: Double?, imgUrl: String?) {
+        _id = id
+        _name = name
+        _description = description
+        _price = price
+        _imgUrl = imgUrl
+    }
+
+    var id: Long?
+        get() = _id
+        set(value) {
+            _id = value
+        }
+
+    var name: String?
+        get() = _name
+        set(value) {
+            _name = value
+        }
+
+    var description: String?
+        get() = _description
+        set(value) {
+            _description = value
+        }
+
+    var price: Double?
+        get() = _price
+        set(value) {
+            _price = value
+        }
+
+    var imgUrl: String?
+        get() = _imgUrl
+        set(value) {
+            _imgUrl = value
+        }
+
+    val categories: Set<Category>
+        get() = categories
+
+    var createdAt: Instant?
+        get() = _createdAt
+        set(value) {
+            _createdAt = value
+        }
+
+    var updatedAt: Instant?
+        get() = _updatedAt
+        set(value) {
+            _updatedAt = value
+        }
+
+    @PrePersist
+    fun prePersist(){
+        this.createdAt = Instant.now()
+    }
+
+    @PreUpdate
+    fun preUpdate(){
+        this.updatedAt = Instant.now()
+    }
+}
