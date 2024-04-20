8a1ef949d7cf60a826af10306c6fa0a991c88441
Jonatan Santos <jonatan.santos@intersite.com.br>
2024-04-20 07:57:50 -0400

    Avanço no projeto de testes

--- src/test/kotlin/org/rask/catalog/repositories/ProductRepositoryTests.kt
+++ src/test/kotlin/org/rask/catalog/repositories/ProductRepositoryTests.kt
@@ -3,7 +3,7 @@ package org.rask.catalog.repositories
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.rask.catalog.Factory
+import org.rask.catalog.tests.Factory
 import org.rask.catalog.entities.Product
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
--- src/test/kotlin/org/rask/catalog/tests/Factory.kt
+++ src/test/kotlin/org/rask/catalog/tests/Factory.kt
@@ -1,21 +1,20 @@
-package org.rask.catalog
+package org.rask.catalog.tests
 
 import org.rask.catalog.dto.ProductDTO
 import org.rask.catalog.entities.Category
 import org.rask.catalog.entities.Product
-import java.time.Instant
 
 class Factory {
     companion object {
         fun createProduct(): Product {
-            var product = Product(1L, "Phone","goodPhone" , 800.0, "https://img.com/img.png")
+            val product = Product(1L, "Phone","goodPhone" , 800.0, "https://img.com/img.png")
             product.categories.add(Category(2L, "Food"))
             product.categories.add(Category(3L, "Water"))
             return product
         }
 
         fun createProductDTO(): ProductDTO {
-            var product = createProduct()
+            val product = createProduct()
             return ProductDTO(product, product.categories)
         }
     }
